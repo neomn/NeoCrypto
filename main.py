fa45ffe4fb8e035ea7aaf1c53b26203c44124ac8
neomn110 <neomn110@gmail.com>
2023-10-17 12:19:57 +0330

    feat: generate key pairs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from neocrypto.GeneratePrivateKey import PrivateKey
+from neocrypto.GeneratePublicKey import PublicKey
 
-print(PrivateKey('/run/media/neo/Neo/Projects/Programing/other/NeoCrypto/private.text',499))
+# print(PrivateKey('/run/media/neo/Neo/Projects/Programing/other/NeoCrypto/private.txt',499))
+print(PublicKey('/run/media/neo/Neo/Projects/Programing/other/NeoCrypto/public.txt',499))
