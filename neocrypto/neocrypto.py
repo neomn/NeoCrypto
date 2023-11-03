73d4d4303f178ecced94c79ded49c26f786696c4
neomn110 <neomn110@gmail.com>
2023-11-03 15:58:36 +0330

    feat: add key and pub_key to NeoCrypto

--- neocrypto/neocrypto.py
+++ neocrypto/neocrypto.py
@@ -6,11 +6,13 @@ from .Encrypt          import Encrypt
 class NeoCrypto:
     
     def __init__(self, mod_value: int = 499):
-        self.mod_value = mod_value
+        self.mod_value   = mod_value
         self.private_key = PrivateKey(mod_value=mod_value)
-        self.public_key = PublicKey(self.private_key, self.mod_value)
-
+        self.public_key  = PublicKey(self.private_key, self.mod_value)
+        self.key         = self.private_key.generate()
+        self.pub_key     = self.public_key.generate()
     
+
     def encrypt(self, message: str):
         return Encrypt(message=message, public_key=self.public_key)
 
