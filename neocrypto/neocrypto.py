7dd4b51258e506f5c50a7cf91eec87da110de75d
neomn110 <neomn110@gmail.com>
2023-11-04 19:59:36 +0330

    refactor: rename keys and add comments

--- neocrypto/neocrypto.py
+++ neocrypto/neocrypto.py
@@ -1,5 +1,6 @@
-from .PrivateKey import PrivateKey
-from .PublicKey  import PublicKey
+from .Helpers     import read_from_file, save_to_file
+from .PrivateKey import *
+from .PublicKey  import *
 from .Encrypt    import encrypt
 from .Decrypt    import decrypt
 
@@ -8,15 +9,19 @@ class NeoCrypto:
     
     def __init__(self, mod_value: int = 499):
         self.mod_value   = mod_value
-        self.private_key = PrivateKey(mod_value=mod_value)
-        self.public_key  = PublicKey(self.private_key, self.mod_value)
-        self.key         = self.private_key.generate()
-        self.pub_key     = self.public_key.generate()
+        self.private_key = PrivateKey(mod_value=mod_value)             # private key instance
+        self.public_key  = PublicKey(self.private_key, self.mod_value) # public key instance
+        self.k           = self.private_key.generate()                 # stringified private key
+        self.pk          = self.public_key.generate()                  # stringified public key
     
-
+        
+#--------------------------------------------------------------------------
     def encrypt(self, message: str):
         return encrypt(message=message, public_key=self.public_key)
 
         
     def decrypt(self, encrypted_message: list):
         return decrypt(encrypted_message, self.private_key)
+
+    
+
