83363447c8044fadbe09c8f5a5a63e0737619c14
neomn110 <neomn110@gmail.com>
2023-11-04 20:00:25 +0330

    feat: write private key IO functions

--- neocrypto/neocrypto.py
+++ neocrypto/neocrypto.py
@@ -24,4 +24,35 @@ class NeoCrypto:
         return decrypt(encrypted_message, self.private_key)
 
     
+#--------------------------------------------------------------------------
+    def save_private_key(self, file_path: str) -> None:
+        '''
+        save private key in a text file
+        '''
+        save_to_file(self.k, file_path)
+        print('saved :)')
+
+        
+    def read_private_key(self, file_path) -> None:
+        '''
+        read private key from a text file
+        '''
+        content = read_from_file(file_path)
+        key = self.private_key.validate_stringified_key(str(content))
+        self.private_key.key = key
+        self.k = key
+        print('read private key from file successfully')
+        
+
+    def read_private_key_from_string(self, input: str) -> None:
+        '''
+        read private key from user input string
+        '''
+        key = self.private_key.validate_stringified_key(input)
+        self.private_key.key = key
+        self.k = key
+        print('read private key from string successfully')
+
+
+
 
