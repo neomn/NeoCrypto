de65f57038e70874a1a6e141e96f7a43760a4725
neomn110 <neomn110@gmail.com>
2023-11-05 11:26:21 +0330

    feat: add setup.py file

--- neocrypto/Decrypt.py
+++
@@ -1,9 +0,0 @@
-from .PrivateKey                  import PrivateKey
-from .DecryptedCharacterContainer import DecryptedCharacterContainer
-
-def decrypt(encrypted_message: list, private_key: PrivateKey):
-    message = ""
-    for encrypted_ch in encrypted_message:
-        ch = DecryptedCharacterContainer(private_key, encrypted_ch)
-        message += ch.character_ascii_char
-    return message
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="NeoCrypto",
+    version="alpha-0.0.1",
+    description="post quantum asymetric cryptography package",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neomn/NeoCrypto",
+    author="Neo",
+    author_email="neomn110@gmail.com",
+    license="GLP-3.0",
+    classifiers=[
+        "License :: OSI Approved :: GLP License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[""],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
